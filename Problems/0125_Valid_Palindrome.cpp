004439cecc1d1b027d5f3280f6572cd8ea3ae411
Nirob-Barman <niroborin111@gmail.com>
2024-02-22 16:16:30 +0600

    125. Valid Palindrome

---
+++ Problems/0125_Valid_Palindrome.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        int i = 0, j = s.size() - 1;
+
+        while (i < j)
+        {
+            if (!isalnum(s[i]))
+            {
+                i++;
+            }
+            else if (!isalnum(s[j]))
+            {
+                j--;
+            }
+            else if (tolower(s[i]) != tolower(s[j]))
+            {
+                return false;
+            }
+            else
+            {
+                i++;
+                j--;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout << s.isPalindrome("A man, a plan, a canal: Panama") << endl; // true
+    cout << s.isPalindrome("race a car") << endl;                     // false
+    return 0;
+}
