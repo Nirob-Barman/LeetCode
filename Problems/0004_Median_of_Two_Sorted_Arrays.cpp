4d6f895fb0276bee954d36e8b7fe9e4b17aab59a
Nirob-Barman <niroborin111@gmail.com>
2024-03-03 22:19:43 -0800

    4. Median of Two Sorted Arrays

--- Problems/0004_Median_of_Two_Sorted_Arrays.cpp
+++ Problems/0004_Median_of_Two_Sorted_Arrays.cpp
@@ -7,33 +7,49 @@ class Solution
 public:
     double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     {
-        int n = nums1.size();
-        int m = nums2.size();
+        // int n = nums1.size();
+        // int m = nums2.size();
 
-        int i = 0, j = 0;
+        // int i = 0, j = 0;
+
+        // vector<int> v;
+
+        // while (i < n && j < m)
+        // {
+        //     if (nums1[i] < nums2[j])
+        //         v.push_back(nums1[i++]);
+        //     else
+        //         v.push_back(nums2[j++]);
+        // }
+
+        // while (i < n)
+        //     v.push_back(nums1[i++]);
+
+        // while (j < m)
+        //     v.push_back(nums2[j++]);
+
+        // // for (int i = 0; i < v.size(); i++)
+        // //     cout << v[i] << " ";
+        // // cout << endl;
+
+        // if (v.size() % 2 == 0)
+        //     return (v[v.size() / 2] + v[v.size() / 2 - 1]) / 2.0;
+
+        // return v[v.size() / 2];
 
         vector<int> v;
 
-        while (i < n && j < m)
-        {
-            if (nums1[i] < nums2[j])
-                v.push_back(nums1[i++]);
-            else
-                v.push_back(nums2[j++]);
-        }
+        for (auto num : nums1)
+            v.push_back(num);
+        for (auto num : nums2)
+            v.push_back(num);
 
-        while (i < n)
-            v.push_back(nums1[i++]);
-
-        while (j < m)
-            v.push_back(nums2[j++]);
-
-        // for (int i = 0; i < v.size(); i++)
-        //     cout << v[i] << " ";
-        // cout << endl;
+        sort(v.begin(), v.end());
 
         if (v.size() % 2 == 0)
+        {
             return (v[v.size() / 2] + v[v.size() / 2 - 1]) / 2.0;
+        }
 
         return v[v.size() / 2];
     }
