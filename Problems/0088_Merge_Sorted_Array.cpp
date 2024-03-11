e58e4d889c69a1934d04afb811a674e4a82b3efe
Nirob-Barman <niroborin111@gmail.com>
2024-03-11 15:14:51 +0600

    88. Merge Sorted Array

--- Problems/0088_Merge_Sorted_Array.cpp
+++ Problems/0088_Merge_Sorted_Array.cpp
@@ -7,11 +7,38 @@ class Solution
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
     {
-        for (int i = 0; i < n; i++)
+        // for (int i = 0; i < n; i++)
+        // {
+        //     nums1[m + i] = nums2[i];
+        // }
+        // sort(nums1.begin(), nums1.end());
+
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0)
         {
-            nums1[m + i] = nums2[i];
+            if (nums1[i] > nums2[j])
+            {
+                nums1[k--] = nums1[i--];
+            }
+            else
+            {
+                nums1[k--] = nums2[j--];
+            }
         }
-        sort(nums1.begin(), nums1.end());
+
+        while (j >= 0)
+        {
+            nums1[k--] = nums2[j--];
+        }
+
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            cout << nums1[i] << " ";
+        }
+        cout << endl;
     }
 };
 
