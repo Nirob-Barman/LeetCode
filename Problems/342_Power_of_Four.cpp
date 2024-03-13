28b8f2ec7f7b4915521219a36c827dbcafdeab07
Nirob-Barman <niroborin111@gmail.com>
2024-03-13 21:32:57 +0600

    342. Power of Four

--- Problems/342_Power_of_Four.cpp
+++ Problems/342_Power_of_Four.cpp
@@ -10,16 +10,34 @@ public:
         if (n <= 0)
             return false;
 
-        while (n % 4 == 0)
-            n /= 4;
+        // while (n % 4 == 0)
+        //     n /= 4;
 
-        return n == 1;
+        // return n == 1;
+
+        double x = log10(n) / log10(4);
+
+        // return floor(x) == ceil(x);
+        // return (x - floor(x) == 0);
+        return (x - int(x) == 0);
     }
 };
 
 int main()
 {
     Solution s;
-    cout << s.isPowerOfFour(16) << endl;  // true
+    cout << s.isPowerOfFour(16) << endl; // true
+    cout << s.isPowerOfFour(5) << endl;  // false
+    cout << s.isPowerOfFour(1) << endl;  // true
+    cout << s.isPowerOfFour(64) << endl; // true
     return 0;
 }
+
+// Log base Change Formula
+// Logb(a) = Logd(a)/Logd(b)
+
+// Given: a^x = M
+// We know that if a^x = M, then log_a(M) = x
+// For example, if 4^3 = 64, then log_4(64) = log_4(4^3) = 3 * log_4(4) = 3
+// Now, we will check if the number is a power of 4 or not.
+// If 64 = 4^3, then log_10(64) / log_10(4) should equal 3.
