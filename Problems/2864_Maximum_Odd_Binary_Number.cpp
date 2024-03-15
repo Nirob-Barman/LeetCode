9fd6a6aaef6fc9fc60ce25b3d68b4e2e0b7d0acc
Nirob-Barman <niroborin111@gmail.com>
2024-03-15 10:42:35 +0600

    2864. Maximum Odd Binary Number

---
+++ Problems/2864_Maximum_Odd_Binary_Number.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string maximumOddBinaryNumber(string s)
+    {
+        // int numOfOnes = s.count(s.begin(), s.end(), '1');
+        int numOfOnes = countOnes(s);
+        int numOfZeros = s.size() - numOfOnes;
+
+        // Hints: return "1" * (numOfOnes - 1) + "0" * numOfZeros + "1";
+        // return string(numOfOnes - 1, '1') + string(numOfZeros, '0') + string(1, '1');
+        return string(numOfOnes - 1, '1') + string(numOfZeros, '0') + "1";
+    }
+
+    int countOnes(string s)
+    {
+        int numOfOnes = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '1')
+                numOfOnes++;
+        }
+
+        return numOfOnes;
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout << s.maximumOddBinaryNumber("1010") << endl;  // "1001"
+    cout << s.maximumOddBinaryNumber("1111") << endl;  // "1111"
+    cout << s.maximumOddBinaryNumber("11011") << endl; // "11101"
+    return 0;
+}
