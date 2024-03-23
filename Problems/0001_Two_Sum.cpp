86d371f3f829c8a413cf91b9417ca591bafc8036
Nirob_Barman <niroborin111@gmail.com>
2024-03-23 08:42:40 +0600

    1. Two Sum

--- Problems/0001_Two_Sum.cpp
+++ Problems/0001_Two_Sum.cpp
@@ -7,15 +7,29 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        for (int i = 0; i < nums.size(); i++)
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     for (int j = i + 1; j < nums.size(); j++)
+        //     {
+        //         if (nums[i] + nums[j] == target)
+        //         {
+        //             return {i, j};
+        //         }
+        //     }
+        // }
+
+        unordered_map<int, int> hashTable;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
         {
-            for (int j = i + 1; j < nums.size(); j++)
+            int complement = target - nums[i];
+            // cout << nums[i] << " " << complement << endl;
+            if (hashTable.count(complement))
             {
-                if (nums[i] + nums[j] == target)
-                {
-                    return {i, j};
-                }
+                return {hashTable[complement], i};
             }
+            hashTable[nums[i]] = i;
         }
         return {-1, -1}; // not found
     }
@@ -27,6 +41,6 @@ int main()
     vector<int> nums = {2, 7, 11, 15};
     int target = 9;
     vector<int> result = s.twoSum(nums, target);
-    cout << result[0] << " " << result[1] << endl;
+    cout << result[0] << " " << result[1] << endl; // 0 1
     return 0;
 }
