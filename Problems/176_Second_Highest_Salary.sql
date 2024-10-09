2cdfaa667c0f0b0626acdc3f9acfc5737a38e04a
Nirob-Barman <niroborin111@gmail.com>
2024-10-09 22:26:32 +0600

    176. Second Highest Salary

---
+++ Problems/176_Second_Highest_Salary.sql
@@ -0,0 +1,11 @@
+use leetcode;
+
+-- Create table If Not Exists Employee (id int, salary int);
+-- Truncate table Employee;
+-- insert into Employee (id, salary) values ('1', '100');
+-- insert into Employee (id, salary) values ('2', '200');
+-- insert into Employee (id, salary) values ('3', '300');
+
+select max(salary) AS SecondHighestSalary 
+from Employee
+where salary < (select max(salary) from Employee);
