5af101688cbeb11dd985caa56c631a6a283265f8
Nirob-Barman <niroborin111@gmail.com>
2024-10-14 07:43:04 +0600

    196. Delete Duplicate Emails

---
+++ Problems/196_Delete_Duplicate_Emails.sql
@@ -0,0 +1,15 @@
+use leetcode;
+
+-- Create table If Not Exists Person (Id int, Email varchar(255));
+-- Truncate table Person;
+-- insert into Person (id, email) values ('1', 'john@example.com');
+-- insert into Person (id, email) values ('2', 'bob@example.com');
+-- insert into Person (id, email) values ('3', 'john@example.com');
+
+select p1.id, p1.email
+from Person p1, Person p2
+where p1.email = p2.email and p1.id > p2.id;
+
+delete p1
+from Person p1, Person p2
+where p1.email = p2.email and p1.id > p2.id;
