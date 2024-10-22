3ee45201a9bbbc6f4410d37700f19d06932ed807
Nirob-Barman <niroborin111@gmail.com>
2024-10-22 08:09:55 +0600

    586. Customer Placing the Largest Number of Orders

--- Problems/586_Customer_Placing_the_Largest_Number_of_Orders.sql
+++ Problems/586_Customer_Placing_the_Largest_Number_of_Orders.sql
@@ -1,7 +1,15 @@
 use leetcode;
 
-SELECT customer_number 
-FROM orders
-GROUP BY customer_number
-ORDER BY COUNT(customer_number) DESC 
-LIMIT 1;
+-- Create table If Not Exists orders (order_number int, customer_number int);
+-- Truncate table orders;
+-- insert into orders (order_number, customer_number) values ('1', '1');
+-- insert into orders (order_number, customer_number) values ('2', '2');
+-- insert into orders (order_number, customer_number) values ('3', '3');
+-- insert into orders (order_number, customer_number) values ('4', '3');
+
+select customer_number 
+from orders
+group by customer_number
+order by COUNT(customer_number) desc
+-- order by COUNT(*) desc
+limit 1;
