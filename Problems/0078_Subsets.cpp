046683c7c2ba2f81bafe650eb10faae1b0d1ab69
Nirob-Barman <niroborin111@gmail.com>
2025-05-26 07:28:18 +0600

    78. Subsets

--- Problems/0078_Subsets.cpp
+++ Problems/0078_Subsets.cpp
@@ -64,3 +64,27 @@ int main()
 // 1 << 3 = 1000
 // 1 << 4 = 10000
 // it follows 2^n
+
+// Each number i in that range represents a subset based on its binary representation:
+//
+//  i   Binary   Meaning             Subset
+// ----------------------------------------------
+//  0   000      no bits set         []
+//  1   001      only 0th bit set    [1]
+//  2   010      only 1st bit set    [2]
+//  3   011      0th and 1st         [1, 2]
+//  4   100      only 2nd bit set    [3]
+//  5   101      0th and 2nd         [1, 3]
+//  6   110      1st and 2nd         [2, 3]
+//  7   111      all bits set        [1, 2, 3]
+
+// Example: nums = [1, 2, 3], i = 5 (binary 101)
+// This loop checks each bit of i to decide which nums[j] to include.
+//
+// j   1 << j (binary)   i (5 = 101) & (1 << j)     Bit set?   Include nums[j]?
+// ------------------------------------------------------------------------------
+// 0   001               101 & 001 = 001 (true)     Yes        nums[0] = 1
+// 1   010               101 & 010 = 000 (false)    No         (skip)
+// 2   100               101 & 100 = 100 (true)     Yes        nums[2] = 3
+//
+// Final subset generated: [1, 3]
